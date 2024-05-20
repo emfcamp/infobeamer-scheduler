74482ab127bc29b54f92ff8b65862bb9aa6bbb0c
Matthew Stratford <hi@mstratford.net>
2024-05-20 22:33:56 +0100

    Scale abstract based on number of lines at max font size, so we can fit big blurbs.

--- frab/tile.lua
+++ frab/tile.lua
@@ -340,10 +340,11 @@ local function view_next_talk(starts, ends, config, x1, y1, x2, y2, events)
 
         -- Abstract
         if abstract then
+            local initial_abstract_size = abstract_size
             local lines = wrap(current_talk.abstract, font, abstract_size, a.width - col2)
             -- try and make the abstrack smaller till it fits on the screen nicely.
             local max_lines = 6
-            while (#lines > max_lines and abstract_size > 30) do
+            while ((abstract_size * #lines > (initial_abstract_size * max_lines)) and (abstract_size > 40)) do
                 abstract_size = math.floor(abstract_size * 0.8)
                 lines = wrap(current_talk.abstract, font, abstract_size, a.width - col2)
             end
