c4fab51a749812c26f2ae13c2ffa10eb57982f57
Matthew Stratford <hi@mstratford.net>
2024-05-25 12:06:19 +0100

    Tiny more alignment

--- frab/tile.lua
+++ frab/tile.lua
@@ -539,7 +539,7 @@ local function view_event_list(starts, ends, config, x1, y1, x2, y2, events)
 
         -- "/u1F39F" 🎫 U+1F3AB
         -- Requires ticket
-        text(x+split_x-120, y+(track_text_size*2)+20, "🎫", track_text_size, rgba({245, 116, 20},1))
+        text(x+split_x-117, y+(track_text_size*2)+20, "🎫", track_text_size, rgba({245, 116, 20},1))
         -- title
         for idx = 1, #title_lines do
             text(x+split_x, y, title_lines[idx], title_size, rgba(default_color,1))
