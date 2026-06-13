82694a6dd8d34e3dd5588b1ee592d8d380b13fb7
Matthew Stratford <hi@mstratford.net>
2026-06-13 02:39:14 +0100

    EMF has a lot of events, bump the numbers again to get more event types during day time into evening.

--- frab/tile.lua
+++ frab/tile.lua
@@ -257,7 +257,7 @@ local function check_next_talk(config)
         if filter_23_hrs then
             is_within_time = talk.start_unix < now + (60*60*23)
         end
-        if talk.start_unix > now and (is_within_time) and #next_talks < 50 then
+        if talk.start_unix > now and (is_within_time) and #next_talks < 100 then
 
             next_talks[#next_talks+1] = talk
             -- Have a separate list of events attendee submitted (not from the approved call for participation)
