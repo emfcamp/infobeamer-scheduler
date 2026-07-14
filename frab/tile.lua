b5f7ea913ea6ff281e70b35528ea9cefdf5ca667
Matthew Stratford <hi@mstratford.net>
2026-07-14 18:08:34 +0100

    DEBUG: Print track info

--- frab/tile.lua
+++ frab/tile.lua
@@ -588,6 +588,8 @@ local function view_event_list(starts, ends, config, x1, y1, x2, y2, events)
         local track_text_size = math.floor(time_size * 0.7)
         -- track title
         if talk.track then
+            print("Track info:")
+            pp(talk.track)
             local width = font:width(talk.track.display_name, track_text_size)+time_size -- Add the width of one time character as a right padding
             text(x+split_x-width, y+time_size, talk.track.display_name, track_text_size, rgba(talk.track.color, 1))
         end
