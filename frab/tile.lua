183965b489444c9c7724691bee996bfbc73fcef9
Matthew Stratford <hi@mstratford.net>
2026-07-17 10:18:46 +0100

    Increase events so we see lots of stuff. EMF is too popular

--- frab/tile.lua
+++ frab/tile.lua
@@ -261,7 +261,7 @@ local function check_next_talk(config)
         if filter_23_hrs then
             is_within_time = talk.start_unix < now + (60*60*23)
         end
-        if talk.start_unix > now and (is_within_time) and #next_talks < 100 then
+        if talk.start_unix > now and (is_within_time) and #next_talks < 1000 then
 
             next_talks[#next_talks+1] = talk
             -- Have a separate list of events attendee submitted (not from the approved call for participation)
